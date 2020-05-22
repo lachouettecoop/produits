a3aaa914a0ed1e24a7672e261463fc71cbeae210
real34 <real34@users.noreply.github.com>
2020-05-22 04:07:40 +0000

    deploy: 2d3c1a9db63cd768639d393d4f4ee22d65234ed0

--- _next/static/vuAoH2nFv01n8tfUcIogf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/preparations":[a],"/preparations/[id]":["static\u002Fchunks\u002Fd7eeaac4.36bb72a62d407315f5f4.js",a]}}("static\u002Fchunks\u002F637b664891f4d1929de8ea5320a657c3946d9625.252aa11d565d257e1e5e.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- js/fermeture.js
+++ js/fermeture.js
@@ -1,4 +1,4 @@
-const CLOSED_TIMESTAMP = 1589983200766;
+const CLOSED_TIMESTAMP = 1590156000822;
 
 function checkClosed() {
   if (Date.now() > CLOSED_TIMESTAMP) {
