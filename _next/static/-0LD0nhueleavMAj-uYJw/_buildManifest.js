cfb97542f5caad94b39aafb56f43cc5f20262b7f
real34 <real34@users.noreply.github.com>
2020-09-10 06:59:26 +0000

    deploy: 383434b09db734aea2201d4d394615bfda362120

---
+++ _next/static/-0LD0nhueleavMAj-uYJw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/preparations":[a],"/preparations/[id]":["static\u002Fchunks\u002Fd7eeaac4.36bb72a62d407315f5f4.js",a]}}("static\u002Fchunks\u002Fb0ed432a71a4fc3d55703f26f8b19d09ddb36588.068c8f79777ce6fe7cb8.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
