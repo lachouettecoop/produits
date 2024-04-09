d1318ff1b2392a421f6d1b8b1a242694b9efce47
mejjjor <mejjjor@users.noreply.github.com>
2024-04-09 19:42:59 +0000

    deploy: 097a9cd54bf43a3c87b32d3bca43efb156d96956

---
+++ _next/static/o5lUZzI3CNFO7vPXkLJrq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/preparations":[a],"/preparations/[id]":["static\u002Fchunks\u002Fd7eeaac4.36bb72a62d407315f5f4.js",a]}}("static\u002Fchunks\u002F628c89bb4b0fd83fdcd629f343f7ca90d739c8b0.068c8f79777ce6fe7cb8.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
