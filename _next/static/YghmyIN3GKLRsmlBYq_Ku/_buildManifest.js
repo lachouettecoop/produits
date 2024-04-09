df77d54eeb43c8d9b92c2ecb53e34500e36f89c0
RubenGres <RubenGres@users.noreply.github.com>
2024-04-09 19:42:14 +0000

    deploy: 35141460f1dbb98d27f3e6e4ce5030cb07f6c074

---
+++ _next/static/YghmyIN3GKLRsmlBYq_Ku/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/preparations":[a],"/preparations/[id]":["static\u002Fchunks\u002Fd7eeaac4.36bb72a62d407315f5f4.js",a]}}("static\u002Fchunks\u002F5de0eedb77265945cc5e31dc3eb5d34cdf934a46.068c8f79777ce6fe7cb8.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
